21aff8fbcd1acc01fe47784fb1ac0f57fff04cf6
Sumaya <sheikhsumaya622@gmail.com>
2026-07-29 13:54:50 +0600

    login form designed

---
+++ app/(auth)/_components/LoginForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ArrowRight, Lock, Mail } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const LoginForm = () => {
+    // console.log(process.env.BACKEND_API_URL)
+  return (
+    <form className="w-full space-y-6">
+    
+      <Card className="p-5 space-y-4">
+        <div className="space-y-2">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-muted-foreground"
+          >
+            Your Email
+          </label>
+          <div className="relative">
+            <Mail className="absolute left-3 top-3 h-5 w-5 text-muted-foreground pointer-events-none" />
+            <Input
+              name="email"
+              type="email"
+              placeholder="you@example.com"
+              className="pl-10 h-11"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-muted-foreground"
+          >
+            Password
+          </label>
+          <div className="relative">
+            <Lock className="absolute left-3 top-3 h-5 w-5 text-muted-foreground pointer-events-none" />
+            <Input
+              name="password"
+              type="password"
+              placeholder="••••••••"
+              className="pl-10 h-11"
+              required
+            />
+          </div>
+        </div>
+
+        <Button
+          type="submit"
+          className="w-full h-11 rounded-full font-semibold gap-2"
+        >
+          {" "}
+          Sign In
+          <ArrowRight className="h-4 w-4" />
+        </Button>
+
+        <div className="text-center">
+          <p className="text-sm text-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <Link
+              href={"/register"}
+              className="text-primary hover:underline font-medium"
+            >
+              Register here
+            </Link>{" "}
+          </p>
+        </div>
+      </Card>
+    </form>
+  );
+};
+
+export default LoginForm;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
 
-const LoginPage = () => {
+import LoginForm from "../_components/LoginForm";
+
+function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="w-full max-w-md space-y-6 rounded-lg border p-8 shadow-lg">
+          {/* Form Text  */}
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold">Welcome Back</h1>
+            <p className="text-gray-500">
+              Enter your credentials to access your account
+            </p>
+          </div>
+
+          {/* Form  */}
+          <LoginForm />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;

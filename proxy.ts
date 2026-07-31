eb8d092b803e1fbe803624626b5cdf4b8a4b70b7
Sumaya <sheikhsumaya622@gmail.com>
2026-08-01 00:07:28 +0600

    authenticated Pages

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,46 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/properties","/login", "/register" ];
+
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
+  const pathname = request.nextUrl.pathname;
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+  //   console.log(decodedToken)
+  let userRole = null;
+
+  if (decodedToken) {
+    userRole = decodedToken.role;
+  }
+// User Login but trying to access login page 
+if(accessToken && AUTH_ROUTES.includes(pathname)){
+    if(userRole === "TENANT"){
+        return NextResponse.redirect(new URL('/tenant-dashboard', request.url))
+    }else if(userRole === "LANDLORD"){
+        return NextResponse.redirect(new URL('/landlord-dashboard', request.url))
+    }else if(userRole === "ADMIN"){
+        return NextResponse.redirect(new URL('/admin-dashboard', request.url))
+    }else{
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 }
- 
+const isPublic = PUBLIC_ROUTES.some((route) => pathname === route || pathname.startsWith(route + "/"));
+
+const isAuthRoute = AUTH_ROUTES.some((route) => pathname === route || pathname.startsWith(route = "/"));
+
+if(!accessToken && !isPublic && !isAuthRoute)  {
+return NextResponse.redirect(new URL("/login", request.url));
+}
+
+return NextResponse.next();
+}
+
 export const config = {
-  matcher: '/dashboard/:path*',
-}
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
+};

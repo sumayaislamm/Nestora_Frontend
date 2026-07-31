44a6c3cbb067436200c03b62f9d604b072e7d6da
Sumaya <sheikhsumaya622@gmail.com>
2026-08-01 01:10:43 +0600

    clear code

--- app/service/getMe.ts
+++ app/service/getMe.ts
@@ -32,36 +32,3 @@ export const getMe = async () =>{
  return result;
 }
 
-// "use server";
-
-// import { cookies } from "next/headers";
-
-// export const getMe = async () => {
-//   const cookieStore = await cookies();
-//   const accessToken = cookieStore.get("accessToken")?.value;
-//   console.log(accessToken)
-
-//   if (!accessToken) {
-//     return { success: false, message: "User not logged in" };
-//   }
-
-//   try {
-//     const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/me`, {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//       },
-//       next: { tags: ["my-profile"] },
-//     });
-
-//     if (!res.ok) {
-//       return { success: false, message: "Failed to fetch profile" };
-//     }
-
-//     const result = await res.json();
-//     console.log("getMe result:", JSON.stringify(result, null, 2));
-//     return result;
-//   } catch (error) {
-//     console.error("getMe failed:", error);
-//     return { success: false, message: "Failed to fetch profile" };
-//   }
-// };
